8bff044add30c710e08f62a73f6a62bdff70a831
Victor Hugo <victorhugo.prog@gmail.com>
2026-02-13 16:35:34 -0300

    criado obj person

---
+++ src/application/Program.java
@@ -0,0 +1,12 @@
+package application;
+
+import entities.Person;
+
+public class Program {
+    public static void main(String[] args) {
+        
+        Person person1 = new Person("Victor Hugo", 22);
+
+        System.out.println(person1);
+    }
+}
